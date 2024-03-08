5d35c178aaf4de548936306044464c2daca51f79
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-07 23:03:59 -0300

    chore: create repository class
    Create repository class and list and create method

---
+++ api/repository.py
@@ -0,0 +1,25 @@
+from django.conf import settings 
+import pymongo
+
+class WeatherRepository:
+    collection = ""
+
+    def __init__(self, name) -> None:
+        self.collection = name
+    
+    def __get_connection(self):
+        client = pymongo.MongoClient(getattr(settings, "MONGO_CONNECTION_STRING"))
+        connection = client[getattr(settings, "MONGO_DATABASE_NAME")]
+        return connection
+    
+    def __get_collection(self):
+        conn = self.__get_connection()
+        collection = conn[self.collection]
+        return collection
+    
+    def list(self):
+        data = self.__get_collection().find({})
+        return data
+    
+    def create(self, document):
+        self.__get_collection().insert_one(document)
