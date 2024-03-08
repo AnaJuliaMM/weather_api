388636c84a518c5154d510ce9f69cbd9901d59e7
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-07 23:06:17 -0300

    feat(urls): create two differents endpoint for both views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import WeatherView
+from .views import WeatherGenerate
 
 
 urlpatterns = [
-    path('', WeatherView.get, name="login")
+    path('', WeatherView.get, name="view"),
+    path('generate', WeatherGenerate.get, name="generate")
+
 ]
