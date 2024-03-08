417a0b733d742f622f7abd05201aae2247461f6e
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-07 23:04:36 -0300

    chore: create view to read mongo collections
    Separate read view from generate data view

--- api/views.py
+++ api/views.py
@@ -5,10 +5,20 @@ from .serializer import TemperatureForecastSerializer
 from  django.http import HttpResponse
 from random import randrange
 from datetime import datetime
+from .repository import WeatherRepository
 
 
 
 class WeatherView(views.View):        
+    def get(request):
+        repository = WeatherRepository('aaa')
+        weathers = repository.list()
+        return render(request, "api/forecasts.html", {"weathers": weathers})
+
+        
+
+
+class WeatherGenerate(views.View):        
     def get(request):
         weathers = []
         for i in range(10):
