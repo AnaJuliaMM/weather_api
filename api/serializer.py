2746ba692c047625ac0a5bb9d088b6db064a6fba
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-20 23:03:39 -0300

    chore: add serializer to validate object model

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import WeatherEntity
 
-class TemperatureForecastSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeatherEntity
-        fields = '__all__'
+class WeatherSerializer(serializers.Serializer):
+    city_name = serializers.CharField(required=False)
+    temperature = serializers.FloatField()
+    atmospheric_pressure = serializers.FloatField(required=False)
+    humidity = serializers.FloatField(required=False)
+    precipitation_percentage = serializers.FloatField(required=False)
+    weather_conditions = serializers.CharField(required=False)
+    date_time = serializers.DateTimeField(required=False)
