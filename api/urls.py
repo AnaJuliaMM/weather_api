c2084ebddec691301e981c4a60e432e2af575f09
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-20 23:04:03 -0300

    feat: add endpoint for insert and delete documents

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,12 @@ from .views import WeatherView
 urlpatterns = [
     path('', WeatherView.login, name="login"),
     path('forecasts/', WeatherView.list, name="forecasts"),
-    path('forecasts/forecast/', WeatherView.create, name="post_forecast"),
+    path('forecasts/forecast/', WeatherView.post_forecast, name="post_forecast"),
+    path('forecasts/insert/', WeatherView.post, name="insert_forecast"),
     path('forecasts/forecast/<str:document_id>/', WeatherView.getById, name="forecast"),
     path('forecasts/<str:attribute>/<str:value>/', WeatherView.filterByAttribute, name="filter"),
-    # path('generate', WeatherGenerate.get, name="generate")
+    path('forecasts/forecast/<str:document_id>/delete', WeatherView.delete, name="delete_by_id"),
+    path('forecasts/delete/', WeatherView.deleteAll, name="delete_all"),
+
 
 ]
