5d19fc112ad981b5f2bd413fecfe1706e19c73c6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-20 23:04:27 -0300

    chore: configure url to hadle and render templates to the endpoints

--- api/views.py
+++ api/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
 from rest_framework.views import View
-from .models import WeatherEntity
-from .serializer import TemperatureForecastSerializer
-from  django.http import HttpResponse
-from random import randrange
-from datetime import datetime
+from rest_framework.response import Response
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from rest_framework import status
+from .serializer import WeatherSerializer
 from .repositories import WeatherRepository
 from .models import WeatherEntity
-
+from datetime import datetime
 
 
 class WeatherView(View):   
@@ -29,30 +28,37 @@ class WeatherView(View):
         weathers = repository.filterByAttribute(attribute, value)
         return render(request, "api/forecasts.html", {"weathers": weathers})
 
-
-
-    
     def post_forecast(request):
         return render(request, "api/post_forecast.html")
-    
-    
-    def create(request):
-        return render(request, "api/post_forecast.html")
-    
 
-
-# class WeatherGenerate(views.View):        
-#     def get(request):
-#         weathers = []
-#         for i in range(10):
-#             weathers.append(
-#                 WeatherEntity(
-#                     temperature=randrange(start=17, stop=40),
-#                     date_time=datetime.now()
-#                 ))
-#         return render(request, "api/forecasts.html", {"weathers": weathers})
-
+    def post(request):
+        data = request.POST
+        model = WeatherEntity(
+            temperature=data['temperature'],
+            date_time=datetime.now(),
+            atmospheric_pressure=data['atmospheric_pressure'],
+            humidity=data['humidity'],
+            precipitation_percentage=data['precipitation_percentage'],
+            weather_conditions=data['weather_conditions'],
+            city_name=data['city_name']
+        )
+        serializer = WeatherSerializer(data=model.__dict__)
+        if serializer.is_valid():
+            document = serializer.validated_data
+            repository = WeatherRepository('forecasts')
+            repository.insert(document)
+            return render(request, "api/forecasts.html", {"weathers": [serializer.data]})
+        return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def delete(request, document_id):
+        repository = WeatherRepository('forecasts')
+        repository.delete(document_id)
+        return render(request, "api/forecasts.html")
+
+    def deleteAll(request):
+        print('delete')
+        repository = WeatherRepository('forecasts')
+        repository.deleteAll()
+        return render(request, "api/forecasts.html")
     
    
- 
