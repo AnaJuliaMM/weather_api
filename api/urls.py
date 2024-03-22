3f65e63f2000f5a40559e697163739354e038a1c
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-21 22:47:51 -0300

    chore: update url to use insert view

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import WeatherView, WeatherTemplateView
+from .views import WeatherView, WeatherInsert
 
 
 urlpatterns = [
     path('', WeatherView.login, name="login"),
     path('forecasts/', WeatherView.list, name="forecasts"),
-    path('forecasts/forecast/', WeatherTemplateView.post_forecast_template, name="post_forecast"),
+    path('forecasts/forecast/', WeatherInsert.as_view(), name="post_forecast"),
     path('forecasts/insert/', WeatherView.post, name="insert_forecast"),
     path('forecasts/forecast/<str:document_id>/', WeatherView.getById, name="forecast"),
     path('forecasts/<str:attribute>/<str:value>/', WeatherView.filterByAttribute, name="filter"),
