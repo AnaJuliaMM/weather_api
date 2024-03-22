a15c1411c818e8f68cbdb2734e84b4be886cd10b
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-21 21:20:12 -0300

    feat(view): serialize and deserialize mongo documents
    Update view methods that was not been serialized

--- api/views.py
+++ api/views.py
@@ -14,41 +14,76 @@ class WeatherView(View):
         return render(request, "api/login.html")
          
     def list(request):
-        repository = WeatherRepository('forecasts')
-        weathers = repository.list()
-        return render(request, "api/forecasts.html", {"weathers": weathers})
+        try:
+            repository = WeatherRepository('forecasts')
+            # Transform Cursor object in a list of documents
+            documents = list(repository.list()) 
+            # Serializer: validate fields
+            serializer = WeatherSerializer(data=documents, many=True)
+            if serializer.is_valid():
+                # Serializer: transform from json to object
+                weathers = serializer.save()
+                return render(request, "api/forecasts.html", {"weathers": weathers})
+            else: 
+                return render(request, "api/forecasts.html", {"error" : serializer.errors})
+        except ValueError as e:
+            return render(request, "api/forecasts.html", {"error" : e})
+        except Exception as e:
+            return render(request, "api/forecasts.html", {"error" : e})
+            
     
     def getById(request, document_id):
-        repository = WeatherRepository('forecasts')
-        weathers = repository.getById(document_id)
-        return render(request, "api/forecasts.html", {"weathers": weathers})
+        try:
+            repository = WeatherRepository('forecasts')
+            documents = list(repository.getById(document_id))
+            serializer = WeatherSerializer(data=documents, many=True)
+            if serializer.is_valid():
+                weather = serializer.save()
+                return render(request, "api/forecasts.html", {"weathers": weather})
+            else: 
+                return render(request, "api/forecasts.html", {"error" : serializer.errors})
+        except ValueError as e:
+            return render(request, "api/forecasts.html", {"error" : e})
+        except Exception as e:
+            return render(request, "api/forecasts.html", {"error" : e})
     
     def filterByAttribute(request, attribute, value):
-        repository = WeatherRepository('forecasts')
-        weathers = repository.filterByAttribute(attribute, value)
-        return render(request, "api/forecasts.html", {"weathers": weathers})
-
-    def post_forecast(request):
-        return render(request, "api/post_forecast.html")
+        try:
+            repository = WeatherRepository('forecasts')
+            documents = list(repository.filterByAttribute(attribute, value))
+            serializer = WeatherSerializer(data=documents, many=True)
+            if serializer.is_valid():
+                weather = serializer.save()
+                return render(request, "api/forecasts.html", {"weathers": weather})
+            else: 
+                return render(request, "api/forecasts.html", {"error" : serializer.errors})
+        except ValueError as e:
+            return render(request, "api/forecasts.html", {"error" : e})
+        except Exception as e:
+            return render(request, "api/forecasts.html", {"error" : e})
 
     def post(request):
-        data = request.POST
-        model = WeatherEntity(
-            temperature=data['temperature'],
-            date_time=datetime.now(),
-            atmospheric_pressure=data['atmospheric_pressure'],
-            humidity=data['humidity'],
-            precipitation_percentage=data['precipitation_percentage'],
-            weather_conditions=data['weather_conditions'],
-            city_name=data['city_name']
-        )
-        serializer = WeatherSerializer(data=model.__dict__)
-        if serializer.is_valid():
-            document = serializer.validated_data
-            repository = WeatherRepository('forecasts')
-            repository.insert(document)
-            return render(request, "api/forecasts.html", {"weathers": [serializer.data]})
-        return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            data = request.POST
+            model = WeatherEntity(
+                temperature=data['temperature'],
+                date_time=datetime.now(),
+                atmospheric_pressure=data['atmospheric_pressure'],
+                humidity=data['humidity'],
+                precipitation_percentage=data['precipitation_percentage'],
+                weather_conditions=data['weather_conditions'],
+                city_name=data['city_name']
+            )
+            serializer = WeatherSerializer(data=model.__dict__)
+            if serializer.is_valid():
+                document = serializer.validated_data
+                repository = WeatherRepository('forecasts')
+                repository.insert(document)
+                return render(request, "api/forecasts.html", {"weathers": [serializer.data]})
+        except ValueError as e:
+            return render(request, "api/forecasts.html", {"error" : e})
+        except Exception as e:
+            return render(request, "api/forecasts.html", {"error" : e})
     
     def delete(request, document_id):
         repository = WeatherRepository('forecasts')
@@ -56,9 +91,12 @@ class WeatherView(View):
         return render(request, "api/forecasts.html")
 
     def deleteAll(request):
-        print('delete')
         repository = WeatherRepository('forecasts')
         repository.deleteAll()
         return render(request, "api/forecasts.html")
     
+class WeatherTemplateView(View):
+    def post_forecast_template(request):
+        return render(request, "api/post_forecast.html")
    
+
