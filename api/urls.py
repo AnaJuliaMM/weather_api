3492a93b26e3f3614c428f0c16377dfa03bb0bf6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-21 22:55:08 -0300

    feat: remove insert endpoint
    Remove dedicated insert endpoint and use insert view for get and post method

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', WeatherView.login, name="login"),
     path('forecasts/', WeatherView.list, name="forecasts"),
     path('forecasts/forecast/', WeatherInsert.as_view(), name="post_forecast"),
-    path('forecasts/insert/', WeatherView.post, name="insert_forecast"),
     path('forecasts/forecast/<str:document_id>/', WeatherView.getById, name="forecast"),
     path('forecasts/<str:attribute>/<str:value>/', WeatherView.filterByAttribute, name="filter"),
     path('forecasts/forecast/<str:document_id>/delete', WeatherView.delete, name="delete_by_id"),
